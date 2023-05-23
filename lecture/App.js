9a6d48beec27a61376d7d7e1c992c890dab7854f
RizkyMaulita <rizky.maulita@gmail.com>
2023-05-23 23:30:49 +0700

    init

---
+++ lecture/App.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, Image, ImageBackground } from "react-native";
+import backgroundImg from "./assets/bg.png";
+import characterImg from "./assets/mametchi.png";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.containerSafeArea}>
+      <ImageBackground source={backgroundImg} style={styles.background}>
+        <Text style={styles.topText}>TamaGotchiApp</Text>
+        <Image source={characterImg} />
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerSafeArea: {
+    flex: 1,
+    backgroundColor: "#5C3526",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  character: {
+    width: 400,
+    height: 400,
+    opacity: 1,
+    resizeMode: "contain",
+  },
+  topText: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 20,
+    color: "#42261c",
+    margin: 10,
+    position: "absolute",
+    top: 0,
+  },
+});
